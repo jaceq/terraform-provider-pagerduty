44f4427353abcc7537d4341c56fedd3ae02212db
Jacek Kikiewicz <jacek.kikiewicz@competec.ch>
2019-04-11 18:00:42 +0200

    1

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/terraform-providers/terraform-provider-pagerduty
+module github.com/jaceq/terraform-provider-pagerduty
 
 require (
 	github.com/blang/semver v3.5.1+incompatible // indirect
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/terraform-providers/terraform-provider-pagerduty/pagerduty"
+	"github.com/jaceq/terraform-provider-pagerduty/pagerduty"
 )
 
 func main() {
